3219afdeca0b4258034a8f8008e22fb135444e0f
ericzhang <ericzhang.buaa@gmail.com>
2013-01-21 09:22:38 +0800

    完善生成文章脚本

--- bin/process_articles.js
+++ bin/process_articles.js
@@ -5,8 +5,13 @@ var fs = require('fs');
 
 // 读取配置信息
 var siteRoot = process.argv[2];
-var globalConfig = require(siteRoot + '/site.json');
-var articlesConfig = require(siteRoot + '/articles/articles.json');
+if (fs.existsSync(siteRoot + '/site.json') && fs.existsSync(siteRoot + '/articles/articles.json')) {
+    var globalConfig = require(siteRoot + '/site.json');
+    var articlesConfig = require(siteRoot + '/articles/articles.json');
+} else {
+    console.log('[ERROR]Config files missing.');
+    process.exit(1);
+}
 
 // 检测并读取模板文件
 if (fs.existsSync(siteRoot + '/articles/articles.template')) {
@@ -32,17 +37,17 @@ if (dd.length === 1) {
 var updatedOn = yyyy + '-' + mm + '-' + dd;
 
 // 根据配置生成文章html页面
-articlesConfig.articles.forEach(function(article) {
+articlesConfig.articles.forEach(function (article) {
     console.log('[PROCESSING]Generate article: ' + article.id + '.');
-    if (fs.existsSync(siteRoot + '/articles/' + article.id + '.html.meta')) {
-        // 只生成meta文件mtime或模板文件的mtime晚于html文件mtime的文章
-        var meta = fs.readFileSync(siteRoot + '/articles/' + article.id + '.html.meta');
-        var metaStat = fs.statSync(siteRoot + '/articles/' + article.id + '.html.meta');
+    if (fs.existsSync(siteRoot + '/articles/' + article.id + '.html.text')) {
+        // 只生成text文件mtime或模板文件的mtime晚于html文件mtime的文章
+        var text = fs.readFileSync(siteRoot + '/articles/' + article.id + '.html.text');
+        var textStat = fs.statSync(siteRoot + '/articles/' + article.id + '.html.text');
         var skip = false;
         if (fs.existsSync(siteRoot + '/articles/' + article.id + '.html')) {
             var htmlStat = fs.statSync(siteRoot + '/articles/' + article.id + '.html');
             if (htmlStat.mtime.getTime() > templateStat.mtime.getTime() && 
-                htmlStat.mtime.getTime() > metaStat.mtime.getTime()) {
+                htmlStat.mtime.getTime() > textStat.mtime.getTime()) {
                 skip = true;
                 console.log('[PROCESSING]Not modified.');
             }
@@ -57,7 +62,7 @@ articlesConfig.articles.forEach(function(article) {
             t = t.replace(/{%= ARTICLE_POSTED_ON %}/g, article.postedOn);
             t = t.replace(/{%= ARTICLE_UPDATED_ON %}/g, updatedOn);
             t = t.replace(/{%= ARTICLE_LINK %}/g, '/articles/' + article.id + '.html');
-            t = t.replace(/{%= ARTICLE_CONTENT %}/g, meta);
+            t = t.replace(/{%= ARTICLE_CONTENT %}/g, text);
             t = t.replace(/{%= COPYRIGHT_BEGINYEAR %}/g, globalConfig.copyright.beginYear);
             t = t.replace(/{%= COPYRIGHT_ENDYEAR %}/g, globalConfig.copyright.endYear);
             t = t.replace(/{%= COPYRIGHT_OWNER %}/g, globalConfig.copyright.owner);
